32a7b6d343184d25d1a711e8775844d23dcb62c0
5strmichael <arriolamichael1216@gmail.com>
2026-09-21 12:43:08 -0400

    Update ModMetadata.cs for v1.1.3

--- ModMetadata.cs
+++ ModMetadata.cs
@@ -8,11 +8,11 @@ public record ModMetadata : IModMetadata
     public string Name { get; init; } = "NATO Quartermaster";
     public string Author { get; init; } = "Michael";
     public List<string>? Contributors { get; init; } = ["OpenAI ChatGPT"];
-    public SemanticVersioning.Version Version { get; init; } = new("1.1.1");
+    public SemanticVersioning.Version Version { get; init; } = new("1.1.3");
     public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.3");
     public List<string>? Incompatibilities { get; init; }
     public Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
-    public string? Url { get; init; } = "https://github.com/5strmichael/NATO-Quartermaster-";
+    public string? Url { get; init; }
     public string License { get; init; } = "MIT";
     public bool HasPrepatcher { get; init; } = false;
 }
